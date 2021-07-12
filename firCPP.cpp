8b9b28449fc94c121dfb97c4a04f7791ee2858d1
charliegean <wht905@gmail>
2021-07-12 10:32:31 +0800

    fir cpp

---
+++ firCPP.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+* File Name: firCPP.cpp
+* Creat Time: 2021年07月12日 星期一 10时32分01秒
+* Author :Charliegean
+* Mail :wht905@gmail.com
+*************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
+
